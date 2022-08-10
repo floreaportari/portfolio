b2d094946073322744aedd271cbda5ba724e4e51
Florian Portari <floryanportar@gmail.com>
2022-08-10 16:03:14 +0200

    added a click event on CV icon to download a pdf file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cvBtn = document.querySelector("#cv-btn");
 
-cvBtn.addEventListener("click", () => {
+cvBtn.addEventListener("click", function () {
   cvBtn.setAttribute("href", "images/CV_Florea Portari.pdf");
 });
